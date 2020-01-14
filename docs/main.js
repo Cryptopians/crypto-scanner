c4a57c9aa2194892300b38317db4d687846ba570
Rob Moorman <rob@moori.nl>
2020-01-15 00:26:59 +0100

    Optimized performance

--- docs/main.js
+++ docs/main.js
@@ -1,114 +1,189 @@
-const TICKER = 'btcusdt';
+const ASSET_A = 'btc';
+const ASSET_B = 'usdt';
+const TICKER = `${ASSET_A}${ASSET_B}`;
 const PRICE_THRESHOLD = 1500; // usdt
 const QUANTITY_THRESHOLD = 0.1; // btc
-const WALL_THRESHOLD = 10; // btc
+const WALL_THRESHOLD = 25; // btc
+const MAX_ORDERS_TO_RENDER = 50;
+
+const DEPTH_SNAPSHOT_URL = `https://www.binance.com/api/v1/depth?symbol=${TICKER.toUpperCase()}&limit=1000`;
 
 const UI = {
   ticker: document.getElementById('ticker'),
   bids: document.getElementById('bids'),
   asks: document.getElementById('asks'),
+  currentPrices: $('.current-price'),
+  sellWalls: document.getElementById('sell-walls'),
+  buyWalls: document.getElementById('buy-walls'),
 };
 
-const wsStream = new WebSocket(`wss://stream.binance.com:9443/stream?streams=${TICKER}@ticker`);
+let wsStream;
+let wsDepth;
+let currentPrice;
+let lastUpdateId;
+let sells = {};
+let buys = {};
 
-let price = undefined;
+function Order(price, quantity) {
+  this.price = price;
+  this.quantity = quantity;
+}
 
-wsStream.onerror = (event) => {
-  console.log(event);
-};
+function init() {
+  fetch(DEPTH_SNAPSHOT_URL, {
+    mode: 'no-cors'
+  }).then((res) => {
+    console.log(res);
+  }).catch((err) => console.log(err));
 
-wsStream.onmessage = (event) => {
-  const data = JSON.parse(event.data);
+  initStream();
+  initDepth();
+}
 
-  if (data.stream.endsWith('@ticker')) {
-    price = parseFloat(data.data.c);
+function initStream() {
+  wsStream = new WebSocket(`wss://stream.binance.com:9443/stream?streams=${TICKER}@ticker`);
 
-    ticker.innerHTML = `${TICKER.toUpperCase()} $${price}`;
+  wsStream.onerror = (event) => {
+    console.log(event);
+  };
+
+  wsStream.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    if (data.stream.endsWith('@ticker')) {
+      const close = parseFloat(data.data.c);
+
+      // Update ticker
+      if (currentPrice && close > currentPrice) {
+        ticker.innerHTML = `<span class="buy">$${close}&#9650;</span>`;
+      } else if (currentPrice && close < currentPrice) {
+        ticker.innerHTML = `<span class="sell">$${close}&#9660;</span>`;
+      } else {
+        ticker.innerHTML = `$${close}`;
+      }
+
+      // Update browser title
+      document.title = `${TICKER.toUpperCase()} | $${close}`;
+
+      // Update prices
+      UI.currentPrices.html(`$${close}`);
+
+      // Cache current price in memory
+      currentPrice = close;
+    }
+  };
+}
+
+function initDepth() {
+  wsDepth = new WebSocket(`wss://stream.binance.com:9443/ws/${TICKER}@depth@100ms`);
+
+  wsDepth.onerror = (event) => {
+    console.log(event);
+  };
+
+  wsDepth.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    // Drop any event where u is <= lastUpdateId in the snapshot
+    if (lastUpdateId && data.u <= lastUpdateId) {
+      return;
+    }
+
+    // Update lastUpdateId
+    lastUpdateId = data.u;
+
+    // Update bid(s) in order book
+    for (const bid of data.b) {
+      const order = new Order(parseFloat(bid[0]), parseFloat(bid[1]))
+
+      // If the quantity is 0, remove the price level
+      if (order.quantity === 0) {
+        delete buys[order.price];
+      } else {
+        buys[order.price] = order;
+      }
+    }
+
+    // Update ask(s) in order book
+    for (const ask of data.a) {
+      const order = new Order(parseFloat(ask[0]), parseFloat(ask[1]))
+
+      // If the quantity is 0, remove the price level
+      if (order.quantity === 0) {
+        delete sells[order.price];
+      } else {
+        sells[order.price] = order;
+      }
+
+      // Update the dom
+      render();
+    }
   }
-};
+}
 
-const wsDepth = new WebSocket(`wss://stream.binance.com:9443/ws/${TICKER}@depth`);
+function renderWalls(orderBook, element, className, label, reversed) {
+  let sortedOrders = Object.values(orderBook).sort((a, b) => a.price - b.price);
 
-let asks = [];
-let bids = [];
+  if (reversed) {
+    sortedOrders = sortedOrders.reverse();
+  }
 
-function renderDepth(entries, color, element) {
-  const max = Math.max.apply(Math, entries.map((obj) => obj[0] * obj[1]));
+  const closestEligibleOrder = sortedOrders.find((obj) => {
+    return obj.quantity >= WALL_THRESHOLD;
+  });
+
+  if (closestEligibleOrder) {
+    element.innerHTML = `
+      <div class="alert ${className}">
+        <h5 class="alert-heading">
+          Closest ${label} wall
+        </h5>
+        <p>
+          <span class="price">$${closestEligibleOrder.price}</span>
+          <span class="quantity">${closestEligibleOrder.quantity} ${ASSET_A.toUpperCase()}</span>
+        </p>
+      </div>
+    `;
+  }
+}
+
+function renderOrders(orderBook, className, element, reversed) {
+  let sortedOrders = Object.values(orderBook).sort((a, b) => a.price - b.price);
+
+  if (reversed) {
+    sortedOrders = sortedOrders.reverse();
+  }
 
   let html = '';
 
-  if (entries && entries.length) {
-
-    for (let [price, quantity] of entries) {
-      const total = price * quantity;
-      const perc = Math.round((total / max) * 100);
-      const size = 100 - perc;
-
-      if (quantity >= WALL_THRESHOLD) {
-        html = `<p style="font-weight:bolder;margin:0;padding:0;background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span style="color:${color};">$${price}</span><span style="float:right;">${quantity}</span></p>` + html;
-      }
-
-      html += `<p style="font-weight:lighter;margin:0;padding:0;background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span style="color:${color};">$${price}</span><span style="float:right;">${quantity}</span></p>`;
-    }
+  if (sortedOrders.length > MAX_ORDERS_TO_RENDER) {
+    sortedOrders = sortedOrders.slice(0, MAX_ORDERS_TO_RENDER);
   }
+
+  let highestOrderQuantity = Math.max.apply(Math, sortedOrders.map((obj) => obj.quantity));
+
+  sortedOrders.forEach((obj) => {
+    const percentage = Math.round((obj.quantity / highestOrderQuantity) * 100);
+    const size = 100 - percentage;
+
+    if (obj.quantity >= WALL_THRESHOLD) {
+      html += `<p class="order bold" style="background:linear-gradient(90deg, #off34 ${size}%, #f5f5f5 ${size}%);"><span class="${className}">$${obj.price}</span><span class="quantity">${obj.quantity}</span></p>`;      
+    } else {
+      html += `<p class="order" style="background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span class="${className}">$${obj.price}</span><span class="quantity">${obj.quantity}</span></p>`;
+    }
+  });
 
   element.innerHTML = html;
 }
 
-function replaceDepth(source, entries) {
-  const min = Math.min.apply(Math, entries.map((obj) => obj[0]));
-  const max = Math.max.apply(Math, entries.map((obj) => obj[0]));
+function render() {
+  // Sell orders
+  renderWalls(sells, UI.sellWalls, 'alert-danger', 'sell', false);
+  renderOrders(sells, 'text-danger', UI.bids, false);
 
-  return source;
+  // Buy orders
+  renderWalls(buys, UI.buyWalls, 'alert-success', 'buy', true);
+  renderOrders(buys, 'text-success', UI.asks, true);
 }
 
-wsDepth.onerror = (event) => {
-  console.log(event);
-};
-
-wsDepth.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-
-  if (!price) {
-    return;
-  }
-
-  // Define boundaries for price replacement
-  const minAsks = parseFloat(data.a[0][0]);
-  const maxAsks = parseFloat(data.a[data.a.length - 1][0]);
-  const minBids = parseFloat(data.b[data.b.length - 1][0]);
-  const maxBids = parseFloat(data.b[0][0]);
-
-  const newAsks = data.a.reverse().map((obj) => [parseFloat(obj[0]), parseFloat(obj[1])]);
-  const newBids = data.b.map((obj) => [parseFloat(obj[0]), parseFloat(obj[1])]);
-
-  // Red (asks)
-  asks = asks.concat(newAsks).filter((obj) => {
-    if (obj[0] < minAsks || obj[0] > maxAsks) {
-      return false;
-    }
-
-    if (obj[1] > QUANTITY_THRESHOLD && obj[0] > price && obj[0] < (price + PRICE_THRESHOLD)) {
-      return true;
-    }
-
-    return false;
-  }).sort((a, b) => a[0] - b[0]);
-
-  // Green (bids)
-  bids = bids.concat(newBids).filter((obj) => {
-    if (obj[0] < minBids || obj[0] > maxBids) {
-      return false;
-    }
-
-    if (obj[1] > QUANTITY_THRESHOLD && obj[0] < price && obj[0] > (price - PRICE_THRESHOLD)) {
-      return true;
-    }
-
-    return false;
-  }).sort((a, b) => a[0] - b[0]);
-
-  // Render price entries
-  renderDepth(asks, 'red', UI.asks);
-  renderDepth(bids.reverse(), 'green', UI.bids);
-};
+init();
