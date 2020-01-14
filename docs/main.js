50f6e54ded741f6a03c483e34e036cdf406d417d
Rob Moorman <rob@moori.nl>
2020-01-14 21:06:22 +0100

    Implement sell/buy wall detection

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,7 @@
 const TICKER = 'btcusdt';
+const PRICE_THRESHOLD = 1500; // usdt
+const QUANTITY_THRESHOLD = 0.01; // btc
+const WALL_THRESHOLD = 50 // percentage
 
 const UI = {
   ticker: document.getElementById('ticker'),
@@ -8,6 +11,8 @@ const UI = {
 
 const wsStream = new WebSocket(`wss://stream.binance.com:9443/stream?streams=${TICKER}@ticker`);
 
+let price = undefined;
+
 wsStream.onerror = (event) => {
   console.log(event);
 };
@@ -16,30 +21,45 @@ wsStream.onmessage = (event) => {
   const data = JSON.parse(event.data);
 
   if (data.stream.endsWith('@ticker')) {
-    const close = parseFloat(data.data.c);
+    price = parseFloat(data.data.c);
 
-    ticker.innerHTML = `${TICKER.toUpperCase()} $${close}`;
+    ticker.innerHTML = `${TICKER.toUpperCase()} $${price}`;
   }
 };
 
 const wsDepth = new WebSocket(`wss://stream.binance.com:9443/ws/${TICKER}@depth`);
 
+let asks = [];
+let bids = [];
+
 function renderDepth(entries, color, element) {
-  const max = Math.max.apply(Math, entries.map((obj) => parseFloat(obj[0]) * parseFloat(obj[1])));
+  const max = Math.max.apply(Math, entries.map((obj) => obj[0] * obj[1]));
 
   let html = '';
 
   if (entries && entries.length) {
-    for (const [price, quantity] of entries) {
-      const total = parseFloat(price) * parseFloat(quantity);
+
+    for (let [price, quantity] of entries) {
+      const total = price * quantity;
       const perc = Math.round((total / max) * 100);
       const size = 100 - perc;
 
-      html += `<p style="margin:0;padding:0;background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span style="color:${color};">$${parseFloat(price)}</span><span style="float:right;">${quantity}</span></p>`;
+      if (perc >= WALL_THRESHOLD) {
+        html = `<p style="font-weight:bolder;margin:0;padding:0;background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span style="color:${color};">$${price}</span><span style="float:right;">${quantity}</span></p>` + html;
+      }
+
+      html += `<p style="font-weight:lighter;margin:0;padding:0;background:linear-gradient(90deg, #fff ${size}%, #f5f5f5 ${size}%);"><span style="color:${color};">$${price}</span><span style="float:right;">${quantity}</span></p>`;
     }
   }
 
   element.innerHTML = html;
+}
+
+function replaceDepth(source, entries) {
+  const min = Math.min.apply(Math, entries.map((obj) => obj[0]));
+  const max = Math.max.apply(Math, entries.map((obj) => obj[0]));
+
+  return source;
 }
 
 wsDepth.onerror = (event) => {
@@ -48,9 +68,47 @@ wsDepth.onerror = (event) => {
 
 wsDepth.onmessage = (event) => {
   const data = JSON.parse(event.data);
-  const bids = data.b;
-  const asks = data.a.reverse();
 
-  renderDepth(bids, 'green', UI.bids);
+  if (!price) {
+    return;
+  }
+
+  // Define boundaries for price replacement
+  const minAsks = parseFloat(data.a[0][0]);
+  const maxAsks = parseFloat(data.a[data.a.length - 1][0]);
+  const minBids = parseFloat(data.b[data.b.length - 1][0]);
+  const maxBids = parseFloat(data.b[0][0]);
+
+  const newAsks = data.a.reverse().map((obj) => [parseFloat(obj[0]), parseFloat(obj[1])]);
+  const newBids = data.b.map((obj) => [parseFloat(obj[0]), parseFloat(obj[1])]);
+
+  // Red (asks)
+  asks = asks.concat(newAsks).filter((obj) => {
+    if (obj[0] < minAsks || obj[0] > maxAsks) {
+      return false;
+    }
+
+    if (obj[1] > QUANTITY_THRESHOLD && obj[0] > price && obj[0] < (price + PRICE_THRESHOLD)) {
+      return true;
+    }
+
+    return false;
+  }).sort((a, b) => a[0] - b[0]);
+
+  // Green (bids)
+  bids = bids.concat(newBids).filter((obj) => {
+    if (obj[0] < minBids || obj[0] > maxBids) {
+      return false;
+    }
+
+    if (obj[1] > QUANTITY_THRESHOLD && obj[0] < price && obj[0] > (price - PRICE_THRESHOLD)) {
+      return true;
+    }
+
+    return false;
+  }).sort((a, b) => a[0] - b[0]);
+
+  // Render price entries
   renderDepth(asks, 'red', UI.asks);
+  renderDepth(bids.reverse(), 'green', UI.bids);
 };
